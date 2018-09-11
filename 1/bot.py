2ca1724ffb0fc4d89f6e0aed1a1ec0f2ae656c4c
Denis Trofimov <silaradost@yandex.ru>
2018-09-11 17:19:05 +0300

    Reply given planet constellation position now.

--- 1/bot.py
+++ 1/bot.py
@@ -1,6 +1,8 @@
 # Импортируем нужные компоненты
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import ephem
 import logging
+import datetime
 
 
 # Настройки прокси
@@ -9,14 +11,14 @@ PROXY = {
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}
 }
 
-
-'''Функция, которая соединяется с платформой Telegram, "тело" нашего бота'''
+"""Функция, которая соединяется с платформой Telegram, "тело" нашего бота"""
 def main():
     updater = Updater(open("api.token").readline(), request_kwargs=PROXY)
 
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
 
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("planet", planet))
     dispatcher.add_handler(MessageHandler(Filters.text,echo))
     
     updater.start_polling()
@@ -25,12 +27,28 @@ def main():
 def start(bot, update):
     # print("update " + str(type(update.message)))
     user = update.message.from_user
-    greeting = "Hello {0} {1}!".format(user.first_name, user.last_name)
-    bot.send_message(chat_id=update.message.chat_id, text=greeting)
+    text = "Hello {0} {1}!".format(user.first_name, user.last_name)
+    bot.send_message(chat_id=update.message.chat_id, text=text)
 
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
+"""Reply given planet constellation position now."""
+def planet(bot, update):
+    words = update.message.text.split()
+    text = 'No such planet name!' 
+    if len(words) > 1:
+        words[1] = words[1].lower().capitalize()
+        try:
+            planet_object = getattr(ephem, words[1])
+        except AttributeError:
+            bot.send_message(chat_id=update.message.chat_id, text=text)
+            return
+        planet_position = planet_object(datetime.datetime.now())
+        text = "The planet is now in the {0} constellation.".format(
+            str(ephem.constellation(planet_position))
+            )
+    bot.send_message(chat_id=update.message.chat_id, text=text)
 
 if __name__ == "__main__":
     # Enable logging
