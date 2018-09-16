5038173f6ef631b7b2705f1e6b55bfae5d8a4949
Denis Trofimov <silaradost@yandex.ru>
2018-09-16 18:46:06 +0300

    wordcount https://learn.python.ru/lessons/tasks_lesson2.html?full#2

--- 2/bot.py
+++ 2/bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 # Импортируем нужные компоненты
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import ephem
@@ -11,6 +13,7 @@ PROXY = {
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}
 }
 
+
 """Функция, которая соединяется с платформой Telegram, "тело" нашего бота"""
 def main():
     updater = Updater(open("api.token").readline(), request_kwargs=PROXY)
@@ -19,10 +22,12 @@ def main():
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("planet", planet))
+    dispatcher.add_handler(CommandHandler("wordcount", wordcount))
     dispatcher.add_handler(MessageHandler(Filters.text,echo))
     
     updater.start_polling()
     updater.idle()
+
 
 def start(bot, update):
     # print("update " + str(type(update.message)))
@@ -30,8 +35,11 @@ def start(bot, update):
     text = "Hello {0} {1}!".format(user.first_name, user.last_name)
     bot.send_message(chat_id=update.message.chat_id, text=text)
 
+
+"""Reply echo message."""
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
 
 """Reply given planet constellation position now."""
 def planet(bot, update):
@@ -49,6 +57,23 @@ def planet(bot, update):
             str(ephem.constellation(planet_position))
             )
     bot.send_message(chat_id=update.message.chat_id, text=text)
+
+
+"""Count total words in double quotes."""
+def wordcount(bot, update):
+    text = update.message.text
+    
+    responce = "Insert text in double quotes, please."
+    opening_quote_index = text.find('"')
+    closing_quote_index = text.find('"', opening_quote_index + 1)
+    
+    if opening_quote_index > 0 and opening_quote_index < closing_quote_index:
+        quoted_text = text[opening_quote_index + 1:closing_quote_index]
+        word_list = quoted_text.split()
+        responce = "I count total {0} words.".format(len(word_list))
+
+    bot.send_message(chat_id=update.message.chat_id, text=responce)
+
 
 if __name__ == "__main__":
     # Enable logging
