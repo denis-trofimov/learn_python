4502439cc60a55d3f9222ea21985e7b3c0a674e6
denis-trofimov <silaradost@yandex.ru>
2018-11-05 22:55:40 +0300

    Triple Step

--- interview/triple_step.py
+++ interview/triple_step.py
@@ -1,4 +1,16 @@
-
+def triple_step(n):
+    """ Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3
+    steps at a time. Implement a method to count how many possible ways the child can run up the
+    stairs.
+    """
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 1 + 2 * 2 + 1
+    else:
+        ways = triple_step(n - 1) * n + triple_step(n - 2) * (n - 1) 
 
 if __name__ == '__main__':
     stair = int(input("Staircase steps: "))
