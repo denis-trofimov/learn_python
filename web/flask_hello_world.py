0951d1f8863227485c5662283ed53256bdc8834c
Denis Trofimov <silaradost@yandex.ru>
2019-01-03 23:42:17 +0300

    Add catching errors to request.

--- web/flask_hello_world.py
+++ web/flask_hello_world.py
@@ -12,7 +12,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return get_weather.get_weather(get_weather.CITY_ID, get_weather.APP_ID)
+    try:
+        text_weather = get_weather.get_weather(
+            get_weather.CITY_ID,
+            get_weather.APP_ID
+        )
+        return text_weather
+    except:
+        return "Прогноз сейчас недоступен"
 
 
 if __name__ == "__main__":
